4dde810d21fc2bb123eca82ff7818b1e330241b1
NALA <serafina.setiawan@cs.ui.ac.id>
2024-08-28 12:21:36 +0700

    added pws

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j&0$e(2^o_p_0nf($91x7mt3qs-kkln*qv!+c5#h+8o69&t%ok
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "serafina-nala-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
